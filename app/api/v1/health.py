33db216823b8ce4eedf4d9e83111314c102543cb
IlyasSultanov <sultanvoilias80@gmail.com>
2025-10-04 01:12:07 +0500

    fix: Added docstring

--- app/api/v1/health.py
+++ app/api/v1/health.py
@@ -1,8 +1,14 @@
 from fastapi import APIRouter
 
 
-router = APIRouter(prefix="/api/v1/health", tags=["Health"])
+router = APIRouter(prefix="/health", tags=["Health"])
 
 @router.get("/")
 async def health_check():
+    """
+    Проверка состояния сервиса.
+    
+    Returns:
+        dict: Статус сервиса {"status": "ok"}
+    """
     return {"status": "ok"}
