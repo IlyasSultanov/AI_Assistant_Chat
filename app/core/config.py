9f3f2419b006ba0ff09a286c5a2c01c3a677f2f2
IlyasSultanov <sultanvoilias80@gmail.com>
2025-10-04 01:10:22 +0500

    fix: get database url in env file

--- app/core/config.py
+++ app/core/config.py
@@ -1,8 +1,13 @@
 from pathlib import Path
-from pydantic import BaseModel, Field
+from pydantic import BaseModel
+from pydantic.v1 import Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from dotenv import load_dotenv
+
+load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
 
 class AuthSettings(BaseModel):
     private_key: Path = BASE_DIR / "certs" / "jwt-private.pem"
@@ -14,12 +19,16 @@ class AuthSettings(BaseModel):
     audience: str = "ai-assistant-clients"
 
 
+class DatabaseSettings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=BASE_DIR / ".env", extra="ignore")
+
+    db_url: str = Field(alias="DB_URL", env_file=BASE_DIR / ".env")
+
+
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(env_file=".env")
+    model_config = SettingsConfigDict(env_file=BASE_DIR / ".env", extra="ignore")
     auth_settings: AuthSettings = AuthSettings()
-    db_url: str = Field(
-        alias="database_url",
-    )
+    database: DatabaseSettings = DatabaseSettings()
 
 
 settings = Settings()
